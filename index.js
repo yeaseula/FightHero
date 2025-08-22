075dfac8254d897f2c5bf23d012600b25a5ad90f
yeaseula <kminea@naver.com>
2025-08-22 19:25:10 +0900

    캐릭터 선택 필드 생성

--- index.js
+++ index.js
@@ -2,6 +2,86 @@ import { Character } from "./Character.js";
 
 const $ = (node) => document.querySelector(node);
 
+//캐릭터 선택
+
+const selectBtnPrev = $('.prev-btn');
+const selectBtnNext = $('.next-btn');
+
+// 1페이지에서 이전 버튼 비활성화
+// 2페이지에서 다음 버튼이 Go Fight 텍스트로 변경
+// 1페이지에서 선택을 해야 다음 버튼 누르기 가능
+
+//초기 '이전'버튼은 비활성화
+selectBtnPrev.disabled = true;
+
+
+//riado 버튼 선택시 선택값이 input 필드로 넘어가도록
+const heroAction = document.querySelectorAll('input[name="heroteam"]');
+const heroInput = document.getElementById('heroteam-input')
+
+const monsterAction = document.querySelectorAll('input[name="monsterteam"]');
+const monsterInput = document.getElementById('monsterteam-input')
+
+function CharsSelectFunc(Action,Input) {
+    Action.forEach((btn)=>{
+        btn.addEventListener('click',(e)=>{
+            const charstype = e.currentTarget.value;
+            Input.value = charstype
+        })
+    })
+}
+
+CharsSelectFunc(heroAction,heroInput);
+CharsSelectFunc(monsterAction,monsterInput);
+
+//next 버튼 클릭
+selectBtnNext.addEventListener('click',(e)=>{
+    const isReady = e.currentTarget.classList.contains('go-btn');
+
+    //1페이지에서
+    if(!isReady){
+        if(heroInput.value.length == 0) {
+            alert('용사팀 캐릭터를 골라야함');
+            return;
+        };
+
+        ChangeContainer();
+        selectBtnPrev.disabled = false;
+        selectBtnNext.classList.add('go-btn');
+        e.currentTarget.textContent = '결투하러 가기'
+    }
+
+    //2페이지에서
+    if(isReady) {
+        if(monsterInput.value.length == 0){
+            alert('몬스터팀 캐릭터를 골라야함');
+            return;
+        }
+        $('.character-select').remove();
+    }
+})
+
+//prev 버튼 클릭
+selectBtnPrev.addEventListener('click',(e)=>{
+    ChangeContainer();
+    selectBtnPrev.disabled = true;
+    selectBtnNext.classList.remove('go-btn');
+    selectBtnNext.textContent = '다음'
+})
+
+function ChangeContainer() {
+    const selectContainer = document.querySelectorAll('.select-container');
+    selectContainer.forEach((cont)=>{
+        if(cont.classList.contains('on')) {
+            cont.classList.remove('on')
+        } else {
+            cont.classList.add('on')
+        }
+    })
+}
+
+
+//선택한 캐릭터 객체 생성
 const hero = new Character('hero',100,20)
 const monster = new Character('monster',90,20)
 const characterArr = [hero,monster]
